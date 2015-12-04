24880960e1ee25a26a43185c7c4791294b03559e
rahul-cuelogic <rahul.palake@cuelogic.co.in>
2015-12-04 12:40:47 +0530

    simple program for hapijs

---
+++ happitest/helloworld.js
@@ -0,0 +1,27 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
+
+
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
